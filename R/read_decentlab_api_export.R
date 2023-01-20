207f8c1912321874cd55ed431aeea056910e4942
skgrange <skgrange@gmail.com>
2023-01-20 10:25:06 +0100

    :sparkles: add k96 sensor variable names & sensor_type

--- R/read_decentlab_api_export.R
+++ R/read_decentlab_api_export.R
@@ -56,6 +56,7 @@ read_decentlab_api_export_worker <- function(file, df_site_ranges,
     any(stringr::str_detect(names(df), "vaisala_gmp343")) ~ "vaisala",
     any(stringr::str_detect(names(df), "licor_li850")) ~ "licor",
     any(stringr::str_detect(names(df), "atmos22")) ~ "dl_atm22",
+    any(stringr::str_detect(names(df), "senseair_k96")) ~ "senseair_k96",
     TRUE ~ NA_character_
   )
   
