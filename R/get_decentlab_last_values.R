a9d9058e81f8025506192d9e07afa6110b71212d
Stuart Grange <s.k.grange@gmail.com>
2024-03-31 11:47:13 +0200

    :sparkles: enhance get_decentlab_last_values to run multiple times if the api fails to respond

--- R/get_decentlab_last_values.R
+++ R/get_decentlab_last_values.R
@@ -1,10 +1,22 @@
-#' Function to get observational data from the Decentlab API.
+#' Function to get final observational data from the Decentlab API. 
+#' 
+#' \code{get_decentlab_last_values} can be used to "probe" or "prime" the API to 
+#' ensure the API is active and responding and explore what devices and sensors
+#' a domain (or user) has access to.
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @param domain A Decentlab API domain.  
 #' 
 #' @param key A Decentlab API key for \code{domain}. 
+#' 
+#' @param pause Number of seconds to pause or sleep for if the API fails to
+#' respond. 
+#' 
+#' @param max_times Number of times to attempt to get data from the API if it 
+#' fails to respond. 
+#' 
+#' @param verbose Should the function give messages? 
 #' 
 #' @return Tibble. 
 #' 
@@ -17,7 +29,26 @@
 #' )
 #' 
 #' @export
-get_decentlab_last_values <- function(domain, key) {
+get_decentlab_last_values <- function(domain, key, pause = 5, max_times = 3,
+                                      verbose = FALSE) {
+  
+  # Set up settings for potentially multiple runs
+  settings_rate <- purrr::rate_delay(pause = pause, max_times = max_times)
+  
+  # Wrap worker function
+  get_decentlab_last_values_worker_inconsistent <- purrr::insistently(
+    get_decentlab_last_values_worker, rate = settings_rate, quiet = !verbose
+  )
+  
+  # Get last values, potentially by running the function multiple times
+  df <- get_decentlab_last_values_worker_inconsistent(domain, key)
+  
+  return(df)
+  
+}
+
+
+get_decentlab_last_values_worker <- function(domain, key) {
   
   getLast(domain = domain, apiKey = key) %>% 
     as_tibble() %>% 
